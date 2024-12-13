cc420e90ac2d80304c62c157f0a6e3fe16e3044b
Dhanalaxmi-Musku <135241955+Dhanalaxmi-Musku@users.noreply.github.com>
2024-12-13 20:21:21 +0530

    Added:Ability to find frequency of words in a sentence

--- src/HashtableMain.java
+++ src/HashtableMain.java
@@ -1,9 +1,25 @@
 
 public class HashtableMain {
+	public static int wordFrequency(String sentence) {
+        MyHashTable<String, Integer> frequencyMap = new MyHashTable<>();
+        String[] words = sentence.toLowerCase().split(" ");
+
+        for (String word : words) {
+            Integer frequency = frequencyMap.get(word);
+            frequencyMap.add(word, (frequency == null) ? 1 : frequency + 1);
+        }
+
+        System.out.println("Word Frequencies:");
+        for (String word : words) {
+            System.out.println(word + ": " + frequencyMap.get(word));
+        }
+
+        return frequencyMap.size();
+    }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String sentence1 = "To be or not to be";
+        System.out.println("Sentence 1 Unique Words: " + wordFrequency(sentence1));
 	}
 
 }
